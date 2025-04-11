fbcb6335aebbb1fef978e91bf7be6ca2de48ad67
Diego Andres Gomez Polo <diego.gomez@simetrik.com>
2025-04-11 10:33:11 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# ML4Good Bootcamp Colombia 2025
+
+This repository contains code and resources used during the ML4Good Bootcamp in Colombia, 2025.
---
+++ main.py
@@ -0,0 +1,10 @@
+"""Main module for the ml4good package."""
+
+
+def main() -> None:
+    """Execute the main function."""
+    print("Hello from ml4good!")
+
+
+if __name__ == "__main__":
+    main()
